798e93b725b21a989de06d0eefb19bb4e337892a
Riya Agrawal <riyaag0105@gmail.com>
2024-10-12 17:56:25 +0530

    frontend pages + tailwind config

--- backend/portal/views.py
+++ backend/portal/views.py
@@ -55,3 +55,4 @@ def create_oppurtunity(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
